c71bef36bedeed6eb560f853917eb4181f7b4b86
spherewebgr-git <spherewebgr@gmail.com>
2025-03-17 15:48:02 +0200

    login system

--- rest-app/routes/web.php
+++ rest-app/routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('public.index');
 });
 
-Route::get('/login', function () {
-    return view('public.index');
-});
+Route::get('/app-dashboard', [UsersController::class, 'dashboard'])->middleware('auth')->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
